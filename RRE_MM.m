f7447fc2eceab160089ff83f11bde880734318c0
maresonsuresh <120763155+maresonsuresh@users.noreply.github.com>
2025-05-01 19:59:05 -0400

    Update RRE_MM.m

--- RRE_MM.m
+++ RRE_MM.m
@@ -2,19 +2,14 @@ function RRE_MM()
     % Constants
     vol = 1e-15;
     n_A = 6.023e23;
-    c1 = 1e6 / (n_A * vol);
-    c2 = 1e-4;
-    c3 = 1e-1;
-    S_initial = round(5e-7 * n_A * vol);
-    E_initial = round(2e-7 * n_A * vol);
-    ES_initial = 0;
-    P_initial = 0;
+    k = [1e6 / (n_A * vol), 1e-4, 1e-1];  % [k1, k2, k3]
+    initials = [round(5e-7 * n_A * vol), round(2e-7 * n_A * vol), 0, 0];
     t_end = 50;
     t = linspace(0, t_end, 1000);
 
     % Solve ODEs
-    [t, y] = ode45(@(t,y) michaelis_menten_system(t, y, c1, c2, c3, E_initial), t, [S_initial; ES_initial; E_initial; P_initial]);
-    results = [t, y(:,1), y(:,3), y(:,2), y(:,4)];
+    [t, y] = ode45(@(t,y) michaelis_menten_system(y, k), t, initials);
+    results = [t, y];
 
     % Plot
     figure('Color', 'white');
@@ -25,22 +20,23 @@ function RRE_MM()
     plot(results(:,1), results(:,5), 'LineWidth', 3, 'Color', '#d62728'); % P
     hold off;
     
-    legend({'Substrate (S)', 'Complex (ES)', 'Enzyme (E)', 'Product (P)'}, 'Location', 'best', 'FontSize', 16);
+    legend({'Substrate (S)', 'Complex (ES)', 'Enzyme (E)', 'Product (P)'}, ...
+           'Location', 'best', 'FontSize', 16);
     xlabel('Time (s)', 'FontSize', 14); 
     ylabel('Molecule count', 'FontSize', 14);
     grid on;
     set(gca, 'FontSize', 12, 'GridAlpha', 0.3);
 end
 
-function dydt = michaelis_menten_system(~, y, c1, c2, c3, E)
-    s = y(1);
-    c = y(2);
-    e = y(3);
-    p = y(4);
-    
-    ds_dt = -c1 * E * s + (c1 * s + c2) * c;
-    dc_dt = c1 * E * s - (c1 * s + c2 + c3) * c;
-    de_dt = -c1 * E * s + (c1 * s + c2) * c + c3 * c;
-    dp_dt = c3 * c;
-    dydt = [ds_dt; dc_dt; de_dt; dp_dt];
+function dydt = michaelis_menten_system(y, k)
+    % Unpack variables
+    s = y(1);  % (S)
+    e = y(2);  % (E)
+    c = y(3);  % (ES)
+    %derivatives
+    ds_dt = -k(1) * e * s + k(2) * c;
+    de_dt = -k(1) * e * s + (k(2) + k(3)) * c;
+    dc_dt =  k(1) * e * s - (k(2) + k(3)) * c;
+    dp_dt =  k(3) * c;
+    dydt = [ds_dt; de_dt; dc_dt; dp_dt];
 end
